1b49f7fcd85343602a5b6f10a4cbc0e94f1ab54d
Yannick Lavan <lavan@esa.tu-darmstadt.de>
2022-12-05 18:59:04 +0100

    Add sanity check for BDPI folder

--- BlueLib.mk
+++ BlueLib.mk
@@ -2,6 +2,10 @@ MAKEPATH := $(dir $(lastword $(MAKEFILE_LIST)))
 MODULENAME := BlueLib
 MODULEPATH := $(MAKEPATH)src
 EXTRA_BSV_LIBS += $(MODULEPATH)
-C_FILES += $(wildcard $(MODULEPATH)/BDPI/*.c)
 
 $(info Adding $(MODULENAME) from $(MODULEPATH))
+ifeq ($(wildcard $(MODULEPATH)/BDPI/),)
+$(warning ./src/BDPI/ folder not found. BlueLib packages requiring C functions may not link successfully.)
+else
+C_FILES += $(wildcard $(MODULEPATH)/BDPI/*.c)
+endif
